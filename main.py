8c07a664a21321aebbb5c6f68dde6bb8dc50bf12
RajeshPatibandla <patibandla.phani501@gmail.com>
2025-11-24 22:40:50 +0530

    Added API Key

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-GEMINI_API_KEY = '<YOUR_GEMINI_API_KEY_HERE>'
+GEMINI_API_KEY = 'AIzaSyDnROx1cvFKvVhFKcIckCFzNUpbYx9zyvc'
 GEMINI_MODEL_ENDPOINT = (
     'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-pro:generateContent'
 )
@@ -103,3 +103,4 @@ def convert_excel():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
+
